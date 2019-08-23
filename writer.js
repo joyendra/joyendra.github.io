d3e92a78e5bf03b6e82ee1ade9eeeec610eca9a2
Joyendra Roy Biswas <36339255+joyendra@users.noreply.github.com>
2019-08-23 19:39:18 +0530

    Update writer.js

--- writer.js
+++ writer.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     var typed = new Typed('#typed', {
 
         strings: ['<strong>a Developer...</strong>', '<strong>a Hackathon Freak...</strong>', '<strong>a Tech Enthusiast...</strong>', '<strong>a Debater...</strong>', '<strong>a Photographer...</strong>', '<strong>more than just a name</strong>'],
-        typeSpeed: 100,
-        backSpeed: 100,
-        fadeOut: false,
+        typeSpeed: 50,
+        backSpeed: 75,
+        fadeOut: true,
         loop: true,
         showCursor: false
     });
