af57aac520deef7748245a926337c20542b92cb0
Joyendra Roy Biswas <36339255+joyendra@users.noreply.github.com>
2026-03-25 21:47:41 -0700

    Updated amount and added patron

--- birthday-fundme/assets/js/app.js
+++ birthday-fundme/assets/js/app.js
@@ -28,7 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
     { name: 'Jyona', date: new Date().toLocaleDateString() },
     { name: 'Mehul', date: new Date().toLocaleDateString() },
     { name: 'Athang', date: new Date().toLocaleDateString() },
-    { name: 'Meet G', date: new Date().toLocaleDateString() }
+    { name: 'Meet G', date: new Date().toLocaleDateString() },
+    { name: 'Prateek', date: new Date().toLocaleDateString() }
 ];
 
     // Initially hide the custom donation section and show donation display
